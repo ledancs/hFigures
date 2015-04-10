a3d8e4cad326e4a19dd788798aa706d1926cdbe5
Andres <andres.ledesma@tut.fi>
2015-04-10 18:12:01 +0300

    added first engine version

---
+++ data.js
@@ -0,0 +1,219 @@
+var groups = [
+    {
+        label: "Blood Pressure",
+        measurements:[
+            {
+                min: 90,
+                max: 120,
+                units: "mmHg",
+                label: "Systolic",
+                samples: [
+                    {
+                        timestamp: 1420798224,
+                        value: 132
+                    },
+                    {
+                        timestamp: 1423742720,
+                        value: 133
+                    }
+                ]
+            },
+            {
+                min: 60,
+                max: 80,
+                units: "mmHg",
+                label: "Diastolic",
+                samples: [
+                    {
+                        timestamp: 1420798224,
+                        value: 91
+                    },
+                    {
+                        timestamp: 1423742720,
+                        value: 92
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: "Composition",
+        measurements:[
+            {
+                min: 30,
+                max: 40,
+                units: "",
+                label: "Body Mass Index",
+                samples: [
+                    {
+                        timestamp: 1420798224,
+                        value: 32
+                    },
+                    {
+                        timestamp: 1423742720,
+                        value: 33
+                    }
+                ]
+            },
+            {
+                min: 60,
+                max: 80,
+                units: "cm",
+                label: "Waist Diameter",
+                samples: [
+                    {
+                        timestamp: 1420798224,
+                        value: 65.3
+                    },
+                    {
+                        timestamp: 1423742720,
+                        value: 64.8
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: "Activity",
+        measurements:[
+            {
+                min: 6000,
+                max: 10000,
+                units: "steps",
+                label: "Steps per day",
+                samples: [
+                    {
+                        timestamp: 1420798224,
+                        value: 4952
+                    },
+                    {
+                        timestamp: 1423742720,
+                        value: 5013
+                    }
+                ]
+            },
+            {
+                min: 20,
+                max: 120,
+                units: "minutes per day",
+                label: "Exercise",
+                samples: [
+                    {
+                        timestamp: 1420798224,
+                        value: 20
+                    },
+                    {
+                        timestamp: 1423742720,
+                        value: 0
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: "Lab Tests",
+        measurements:[
+            {
+                min: 0,
+                max: 1.3,
+                units: "mmol/L",
+                label: "HDL (bad cholesterol)",
+                samples: [
+                    {
+                        timestamp: 1420798224,
+                        value: 1.35
+                    },
+                    {
+                        timestamp: 1423742720,
+                        value: 1.32
+                    }
+                ]
+            },
+            {
+                min: 5,
+                max: 8,
+                units: "mmol/L",
+                label: "LDL (good cholesterol)",
+                samples: [
+                    {
+                        timestamp: 1420798224,
+                        value: 7.5
+                    },
+                    {
+                        timestamp: 1423742720,
+                        value: 6.5
+                    }
+                ]
+            },
+            {
+                min: 0,
+                max: 2,
+                units: "mmol/L",
+                label: "Triglycerides",
+                samples: [
+                    {
+                        timestamp: 1420798224,
+                        value: 1.55
+                    },
+                    {
+                        timestamp: 1423742720,
+                        value: 1.62
+                    }
+                ]
+            },
+            {
+                min: 2,
+                max: 5,
+                units: "mmol/L",
+                label: "Cholesterol (total)",
+                samples: [
+                    {
+                        timestamp: 1420798224,
+                        value: 3.5
+                    },
+                    {
+                        timestamp: 1423742720,
+                        value: 3.84
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: "Sleep",
+        measurements:[
+            {
+                min: 7,
+                max: 8,
+                units: "hours",
+                label: "Time in Bed",
+                samples: [
+                    {
+                        timestamp: 1420798224,
+                        value: 6.5
+                    },
+                    {
+                        timestamp: 1423742720,
+                        value: 7
+                    }
+                ]
+            },
+            {
+                min: 6,
+                max: 8,
+                units: "hours",
+                label: "Time actually asleep",
+                samples: [
+                    {
+                        timestamp: 1420798224,
+                        value: 6
+                    },
+                    {
+                        timestamp: 1423742720,
+                        value: 6.5
+                    }
+                ]
+            }
+        ]
+    }
+];

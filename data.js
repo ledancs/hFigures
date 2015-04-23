bc526c5e5cabc42708a970e1b0b8a22d98bd668a
Andres <andres.ledesma@tut.fi>
2015-04-23 17:42:42 +0300

    labels with frame and line to the section

--- data.js
+++ data.js
@@ -1,5 +1,42 @@
 var groups = [
     {
+        label: "Blood Pressure II",
+        measurements:[
+            {
+                min: 90,
+                max: 120,
+                units: "mmHg",
+                label: "Systolic",
+                samples: [
+                    {
+                        timestamp: 1420798224,
+                        value: 132
+                    },
+                    {
+                        timestamp: 1423742720,
+                        value: 133
+                    }
+                ]
+            },
+            {
+                min: 60,
+                max: 80,
+                units: "mmHg",
+                label: "Diastolic",
+                samples: [
+                    {
+                        timestamp: 1420798224,
+                        value: 91
+                    },
+                    {
+                        timestamp: 1423742720,
+                        value: 92
+                    }
+                ]
+            }
+        ]
+    },
+    {
         label: "Blood Pressure",
         measurements:[
             {
@@ -74,6 +111,43 @@ var groups = [
         ]
     },
     {
+        label: "Composition II",
+        measurements:[
+            {
+                min: 30,
+                max: 40,
+                units: "",
+                label: "Body Mass Index",
+                samples: [
+                    {
+                        timestamp: 1420798224,
+                        value: 32
+                    },
+                    {
+                        timestamp: 1423742720,
+                        value: 33
+                    }
+                ]
+            },
+            {
+                min: 60,
+                max: 80,
+                units: "cm",
+                label: "Waist Diameter",
+                samples: [
+                    {
+                        timestamp: 1420798224,
+                        value: 65.3
+                    },
+                    {
+                        timestamp: 1423742720,
+                        value: 64.8
+                    }
+                ]
+            }
+        ]
+    },
+    {
         label: "Activity",
         measurements:[
             {
@@ -112,6 +186,75 @@ var groups = [
     },
     {
         label: "Lab Tests",
+        measurements:[
+            {
+                min: 0,
+                max: 1.3,
+                units: "mmol/L",
+                label: "HDL (bad cholesterol)",
+                samples: [
+                    {
+                        timestamp: 1420798224,
+                        value: 1.35
+                    },
+                    {
+                        timestamp: 1423742720,
+                        value: 1.32
+                    }
+                ]
+            },
+            {
+                min: 5,
+                max: 8,
+                units: "mmol/L",
+                label: "LDL (good cholesterol)",
+                samples: [
+                    {
+                        timestamp: 1420798224,
+                        value: 7.5
+                    },
+                    {
+                        timestamp: 1423742720,
+                        value: 6.5
+                    }
+                ]
+            },
+            {
+                min: 0,
+                max: 2,
+                units: "mmol/L",
+                label: "Triglycerides",
+                samples: [
+                    {
+                        timestamp: 1420798224,
+                        value: 1.55
+                    },
+                    {
+                        timestamp: 1423742720,
+                        value: 1.62
+                    }
+                ]
+            },
+            {
+                min: 2,
+                max: 5,
+                units: "mmol/L",
+                label: "Cholesterol (total)",
+                samples: [
+                    {
+                        timestamp: 1420798224,
+                        value: 3.5
+                    },
+                    {
+                        timestamp: 1423742720,
+                        value: 3.84
+                    }
+                ]
+            }
+        ]
+    },
+    {
+        label: "Lab Tests II",
         measurements:[
             {
                 min: 0,
---
+++ hgraph.js
@@ -0,0 +1,335 @@
+/**
+ * Created by andres on 4/23/15.
+ */
+
+var dataset = groups; // how many measurements per group
+
+var pie = d3.layout.pie().value(function (d) {
+    return d.measurements.length;
+}).sort(null);
+
+pie.padAngle(Math.PI / 105);
+
+var w = 800;
+var h = 800;
+
+var outerRadius = w * 0.25;
+var innerRadius = w * 0.165;
+var labelRadius = w * 0.35;
+
+var vBX0 = (w/2) * -1;
+var vBY0 = (h/2) * -1;
+
+var viewbBoxString = "" + vBX0 + " " + vBY0 + " " + w + " " + h;
+
+var arc = d3.svg.arc()
+    .innerRadius(innerRadius)
+    .outerRadius(outerRadius);
+
+var viewBoxString = "" + ((w/2) * -1).toString() +
+    " " + ((h/2) * -1).toString() +
+    " " + w.toString() +
+    " " + h.toString();
+
+var svg = d3.select("div#hgraph-container")
+    .append("svg")
+    .attr("viewBox", viewbBoxString);
+
+var background = svg.append("circle")
+    .attr({
+        "cx": 0,
+        "cy": 0,
+        "r": labelRadius,
+        "stroke": "none",
+        "fill": "none"
+    });
+
+var pieObjects = pie(dataset);
+
+var hgraph = svg.append("g").attr("class", "hgraph");
+
+var points = [];
+
+var angleUnit = function (startAngle, padAngle, endAngle, n) {
+    var start = startAngle + padAngle/2;
+    var end = endAngle - padAngle/2;
+
+    var space = end - start;
+    var unit = space / (n + 1);
+
+    return unit;
+};
+
+var arcs = hgraph.selectAll("g.arc")
+    .data(pieObjects)
+    .enter()
+    .append("g")
+    .attr("class", "arc");
+
+var pointsPerSection = [];
+var dataByAngles = [];
+
+arcs.append("path")
+    .attr({
+        "fill": "#74c476",
+        //"stroke": "#74c476",
+        "stroke": "none",
+        "d": arc
+    });
+
+arcs.each(function (d, i) {
+
+    dataByAngles.push({
+        groupValues: dataset[i],
+        startAngle: d.startAngle,
+        endAngle: d.endAngle,
+        padAngle: d.padAngle,
+        labelAngle: getAngleAtSlice(d.startAngle, d.endAngle, d.padAngle, 2)
+    });
+
+    pointsPerSection = generatePoints(i, d.startAngle, d.padAngle, d.endAngle);
+    points = points.concat(pointsPerSection);
+});
+
+var groupLabels = hgraph.selectAll("g.groupLabels")
+    .data(dataByAngles)
+    .enter()
+    .append("g")
+    .attr("class", "groupLabels");
+
+var groupLabelsText = groupLabels.append("text")
+    .text(function(d){
+        return d.groupValues.label;
+    })
+    .attr("text-anchor", "middle")
+    .attr("x", function(d){
+        return Math.cos(d.labelAngle) * labelRadius;
+    })
+    .attr("y", function(d){
+        return Math.sin(d.labelAngle) * labelRadius * -1;
+    })
+    .attr("font-size", w * 0.0275)
+    .each(function (d) {
+        var bbox = this.getBBox()
+        d.bbox = bbox;
+    });
+
+var groupLabelsFrame = groupLabels.append("rect")
+    .attr("x", function(d){
+        return d.bbox.x - 5;
+    })
+    .attr("y", function (d) {
+        return d.bbox.y;
+    })
+    .attr("height", function (d) {
+        return d.bbox.height;
+    })
+    .attr("width", function (d) {
+        return d.bbox.width + 10;
+    })
+    .attr({
+        "rx": 5,
+        "ry": 5,
+        "stroke": "grey",
+        "fill": "#d5f5d5",
+        "stroke-width": 1.5
+    })
+    .each(function (d) {
+        d.line = [{
+            x: this.getBBox().x + this.getBBox().width/2,
+            y: this.getBBox().y + this.getBBox().height/2
+        }];
+    });
+
+
+// move them out of the background circle
+groupLabels.attr("transform", function(d, i){
+    var w = this.getBBox().width * 1.065;
+    var h = this.getBBox().height * 1.065;
+
+    w *= Math.cos(d.labelAngle) > 0 ? 1: -1;
+    h *= Math.sin(d.labelAngle) > 0 ? -1: 1;
+
+    d.line.push({
+        x: (Math.cos(d.labelAngle) * outerRadius) - (w/2),
+        y: (Math.sin(d.labelAngle) * outerRadius * -1) - (h/2)
+    });
+
+    return "translate(" + (w/2) + ", " + (h/2) + ")";
+});
+/*
+groupLabels.append("line")
+    .attr("x1", function (d) {
+        return d.line.x1;
+    })
+    .attr("y1", function (d) {
+        return d.line.y1;
+    })
+    .attr("x2", function (d) {
+        return d.line.x2;
+    })
+    .attr("y2", function (d) {
+        return d.line.y2;
+    })
+    .attr({
+        "stroke": "grey",
+        "stroke-width": 1
+    })
+    .style("stroke-dasharray", ("3, 3"));
+*/
+
+var lineFunction = d3.svg.line()
+    .x(function (d) {
+        return d.x;
+    })
+    .y(function (d) {
+        return d.y;
+    })
+    .tension(0)
+    .interpolate("cardinal-closed");
+
+groupLabels.append("path")
+    .attr("d", function (d) {
+        return lineFunction(d.line);
+    })
+    .attr({
+        "stroke": "grey",
+        "stroke-width": 2,
+        "fill": "none"
+    })
+    .attr("stroke-dasharray", function(d){
+        return 3 + " " + 3;
+    })
+    .each(function (d) {
+        d.direction = true;
+    })
+    .call(transition);
+
+function transition(path){
+    path.transition()
+        .duration(1000)
+        .attrTween("stroke-dasharray", function(){
+            var d = d3.select(this).datum();
+            return tweenDash(d.direction);
+        })
+        .each("end", function () {
+            var d = d3.select(this).datum();
+            d.direction = !d.direction;
+            d3.select(this).call(transition);
+        })
+}
+
+function tweenDash(direction){
+    var i = direction ?
+        d3.interpolateString("3,8", "8,3"):
+        d3.interpolateString("8,3", "3,8");
+    return function(t){
+        return i(t);
+    }
+}
+
+groupLabelsFrame.each(function(){
+    this.parentNode.appendChild(this);
+});
+// move the labels on top of the rectangles
+groupLabelsText.each(function(){
+    this.parentNode.appendChild(this);
+});
+
+// polygon
+
+var polygon = hgraph.append("g").attr("class", "polygon").selectAll("polygon")
+    .data([points])
+    .enter()
+    .append("polygon")
+    .attr("points", function(d) {
+        // compute the coordinates
+        return d.join(" ");
+    }).attr({
+        "stroke": "#5b5b5b",
+        "stroke-width": 1,
+        "fill": "grey",
+        "fill-opacity": 0.35
+    });
+
+var pointsGroup = hgraph.append("g").attr("class", "pointsGroup");
+
+var pointsSection = pointsGroup.selectAll("g.pointsSection")
+    .data(pieObjects)
+    .enter()
+    .append("g")
+    .attr("class", "pointsSection");
+
+// select the sample as well
+pointsSection.selectAll("circle")
+    .data(points)
+    .enter()
+    .append("circle")
+    .attr({
+        "fill": "white",
+        "stroke": "#5b5b5b",
+        "stroke-width": 2,
+        "r": 5
+    })
+    .attr("cx", function (d) {
+        return d[0];
+    })
+    .attr("cy", function (d) {
+        return d[1];
+    });
+
+// move the hgraph
+var translate = "translate(" + (w/2) + ", " + (h/2) + ")";
+// flip the y axis
+var scale = "scale(1, -1)";
+// rotate
+var rotate = "rotate(90)";
+// all the transformations
+var transformations = [scale, rotate];
+// apply
+arcs.attr("transform", transformations.join(" "));
+//pointsGroup.attr("transform", scale + " " + rotate);
+//polygon.attr("transform", scale + " " + rotate);
+
+// console.log(points.join(" "));
+
+function generatePoints(i, startAngle, padAngle, endAngle) {
+    var g = dataset[i];
+    // console.log(g.label);
+    // console.log(startAngle);
+    var ms = g.measurements;
+    var n = ms.length;
+    var delta = angleUnit(startAngle, padAngle, endAngle, n);
+    var angle, m, scale, s, v, x, y, r;
+    var pointsInSection= [];
+    for(var j = 0; j < n; j ++){
+        m = ms[j];
+        s = m.samples[1];
+        v = s.value;
+        angle = startAngle + padAngle + ((j + 1) * delta);
+        scale = d3.scale.linear()
+            .domain([m.min, m.max])
+            .range([innerRadius, outerRadius]);
+        r = scale(v);
+        x = Math.cos(angle) * r;
+        y = Math.sin(angle) * r * -1;
+        pointsInSection.push([x, y]);
+    }
+    return pointsInSection;
+}
+
+function getAngleAtSlice(startAngle, endAngle, padAngle, slice){
+    var start = startAngle + padAngle/2;
+    var end = endAngle - padAngle/2;
+    var space = end - start;
+    return startAngle + padAngle + (space / slice);
+}
+
+function angle(startAngle, endAngle) {
+    var centroidAngle = (startAngle + endAngle)/2;
+    // Math.PI (rad) = 180 (deg)
+    // centroidAngle (rad) = x (deg)
+    var a = (centroidAngle * 180) / Math.PI;
+    // return a > 90 ? a - 180 : a;
+    return a;
+}

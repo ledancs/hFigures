01c02dc9a4a3134c16f0b475977a821df1b9f07d
ledancs <andres.ledesma@tut.fi>
2015-05-15 17:28:19 +0300

    first app allows panning and zooming of the hGraph

---
+++ apps/panZoom/hGraphPanZoomApp.js
@@ -0,0 +1,106 @@
+/**
+ * Created by ledesmaf on 15.5.2015.
+ */
+
+/**
+ *
+ * @param {number} n - Number of groups to be animated
+ * @param {function} entryCall - Function to call when the animation starts
+ * @param {function} callback - Function to call when the animation ends
+ * @constructor
+ */
+function GroupedAnimation(n, entryCall, callback){
+    this.n = n;
+    this.elementsDone = 0;
+    this.entryCall = entryCall;
+    this.callback = callback;
+}
+
+GroupedAnimation.prototype.animateOpacity = function (d3element, val) {
+    this.entryCall();
+    var self = this;
+    d3element
+        .transition()
+        .attr("opacity", val)
+        .duration(1300)
+        .call(endAll, function(){ self.check.apply(self); });
+};
+
+function endAll(transition, callback) {
+    if (transition.size() === 0) { callback() }
+    var n = 0;
+    transition
+        .each(function() { ++n; })
+        .each("end", function() { if (!--n) callback.apply(this, arguments); });
+}
+
+// there are several groups that are being animated each with different elements so we
+// need to make sure that all the groups and their elements have finished the animation
+GroupedAnimation.prototype.check = function () {
+    this.elementsDone++;
+    if(this.elementsDone === this.n){
+        this.elementsDone = 0;
+        this.callback();
+    }
+};
+
+GroupedAnimation.prototype.isVisible = function(d3element){
+    var opacity = d3element.attr("opacity");
+    var intOpacity = parseInt(opacity);
+    return intOpacity != 0;
+};
+
+GroupedAnimation.prototype.show = function(d3element){
+    if(!this.isVisible(d3element))
+        this.animateOpacity(d3element, 1);
+};
+
+GroupedAnimation.prototype.dim = function(d3element){
+    if(this.isVisible(d3element))
+        this.animateOpacity(d3element, 0.1);
+};
+
+GroupedAnimation.prototype.hide = function(d3element){
+    if(this.isVisible(d3element))
+        this.animateOpacity(d3element, 0);
+};
+
+function hGraphPanZoomApp(hGraph){
+    // show and hide
+    var groupLabels = hGraph.select("g.labels").selectAll("g.groupLabel");
+    var measurementLabels = hGraph.select("g.labels").selectAll("g.measurementLabel");
+
+    // show and hide
+    measurementLabels.attr("opacity", 0);
+    groupLabels.attr("opacity", 1);
+
+    // this is app specific code
+    // pan and zoom lib
+    // setup the group animations
+    var zoomEntryCall = function () {
+        pzlib.allowZoom = false; // disable zoom
+    };
+    var zoomCallback = function () {
+        pzlib.allowZoom = true; // enable zoom
+    };
+    // two groups, and function to call first and after the groups have finished the animation
+    var ga = new GroupedAnimation(2, zoomEntryCall, zoomCallback);
+    // zoom and pan
+    pzlib.setup("hGraph-container");
+    pzlib.deltaS = 0.50;
+    pzlib.zoomLimit = 2.50;
+
+    pzlib.afterZoom = function () {
+        pzlib.zoomLevel > 1 ? zoomInAction(): zoomOutAction();
+    };
+
+    function zoomInAction(){
+        ga.show(measurementLabels);
+        ga.dim(groupLabels);
+    }
+
+    function zoomOutAction(){
+        ga.show(groupLabels);
+        ga.hide(measurementLabels);
+    }
+}

6544fc8c40309eed16ae1f972f77005f7bdc6aad
ledancs <andres.ledesma@tut.fi>
2015-05-19 17:52:28 +0300

    Added d3 pan and zoom.

--- apps/d3zoom/zoom.js
+++ apps/d3zoom/zoom.js
@@ -4,16 +4,57 @@
 
 // testing
 function d3zoom(w, h){
+    var prevScale = 1;
+    var zoomedIn = false;
+
+    function zoomIn(scale1, scale2){
+        return scale1 > 1.7 && scale1 > scale2 && !zoomedIn;
+    }
+
+    function zoomOut(scale1, scale2){
+        return scale1 <= 1.7 && scale1 < scale2 && zoomedIn;
+    }
+
+    function toggle(){
+        zoomedIn = !zoomedIn;
+        groupLabels.attr("opacity", zoomedIn ? 0.2: 1);
+        measurementLabels.attr("opacity", zoomedIn ? 1: 0);
+    }
+
     function zoomed() {
+        if(zoomIn(d3.event.scale, prevScale) || zoomOut(d3.event.scale, prevScale))
+            toggle();
+
         hGraphWrapper.attr("transform", "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")");
+        prevScale = d3.event.scale;
     }
 
     var zoom = d3.behavior.zoom()
         .scaleExtent([1, 3])
         .on("zoom", zoomed);
+
     var svg = d3.select("div#hGraph-container")
         .select("svg");
+
     svg.attr("viewBox", "" + 0 + " " + 0 + " " + w + " " + h)
         .call(zoom);
+
     var hGraphWrapper = svg.select("g.hGraph-wrapper");
+    var groupLabels = hGraph.select("g.labels").selectAll("g.groupLabel");
+
+    groupLabels.selectAll("rect")
+        .attr({
+            "stroke-width": 1
+        });
+
+    groupLabels.selectAll("line")
+        .attr({
+            "stroke-width": 1
+        });
+
+    var measurementLabels = hGraph.select("g.labels").selectAll("g.measurementLabel");
+
+    // show and hide
+    measurementLabels.attr("opacity", 0);
+    groupLabels.attr("opacity", 1);
 }
